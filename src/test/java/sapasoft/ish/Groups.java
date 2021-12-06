aa2da64c75431f8bdfdfbac0bb8eb27200374931
Кан Сергей <sergik-199022@mail.ru>
2021-12-06 18:29:53 +0600

    ish

--- src/test/java/sapasoft/ish/Groups.java
+++ src/test/java/sapasoft/ish/Groups.java
@@ -1,5 +1,7 @@
 package sapasoft.ish;
 
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.junit4.DisplayName;
 import sapasoft.ish.pages.Ish;
 import sapasoft.ish.testconfigs.Base;
 import org.junit.FixMethodOrder;
@@ -7,6 +9,7 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import static com.codeborne.selenide.Selenide.confirm;
+import static com.codeborne.selenide.Selenide.sleep;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Groups extends Base {
@@ -17,8 +20,12 @@ public class Groups extends Base {
     private String newCode= "newGroup"+System.currentTimeMillis(); // Код группы для редактирования
     private String existcode= "_Test_Group_"; // Код группы, которая существует в системе
     private String intcode = "ТестГрупСерик";  // Код группы, которая используется во взаимодействии
+    private String deleteGroup ="newGroup1638789556177";  // Код группы, которую нужно удалить
+    private String editGroup ="newGroup163";  // Код группы, которую нужно изменить
+
 
     // Создание новой группы
+    @DisplayName("Добавление новой группы")
     @Test
     public void t1CreateNewGroup(){
         Ish ish = new Ish();
@@ -27,19 +34,20 @@ public class Groups extends Base {
         ish.groups().add();
         ish.groups().fillCodeOfGroup(code);
         ish.groups().save();
-        ish.groups().checkGroupWasCreated(code);
+        ish.groups().search(code);
         ish.logOut();
 
     }
 
     // Проверка невозможности создания группы с имеющимся кодом
+    @DisplayName("Проверка невозможности создания группы с одинаковым кодом")
     @Test
     public void t2TryToCreateExistCode(){
         Ish ish = new Ish();
         ish.logIn(login, password);
         ish.groups().open();
         ish.groups().add();
-        ish.groups().fillExistCode();
+        ish.groups().fillCodeOfGroup(existcode);
         ish.groups().save();
         ish.groups().checkGroupExist();
         ish.groups().cancel();
@@ -47,7 +55,7 @@ public class Groups extends Base {
         ish.logOut();
     }
 
-    // Проверка невозможности сохранения имеющегося кода при редактировании
+    @DisplayName("Проверка невозможности сохранения группы с одинаковым кодом при редактировании")
     @Test
     public void t3TryEditExistCodeGroup(){
         Ish ish = new Ish();
@@ -55,7 +63,7 @@ public class Groups extends Base {
         ish.groups().open();
         ish.groups().edit();
         ish.groups().clearFieldGroup();
-        ish.groups().fillExistCode();
+        ish.groups().fillCodeOfGroup(existcode);
         ish.groups().save();
         ish.groups().checkGroupExist();
         ish.groups().cancel();
@@ -64,6 +72,7 @@ public class Groups extends Base {
     }
 
     // Проверка создания пустой формы группы
+    @DisplayName("Проверка невозможности создания группы с пустым полем код")
     @Test
     public void t4CreateEmptyFieldCode(){
         Ish ish = new Ish();
@@ -77,6 +86,7 @@ public class Groups extends Base {
     }
 
     // Проверка сохранения пустой формы группы при редактировании
+    @DisplayName("Проверка невозможности сохранения группы с пустым полем код при редактировании")
     @Test
     public void t5EditEmptyFieldCode(){
         Ish ish = new Ish();
@@ -92,17 +102,19 @@ public class Groups extends Base {
     }
 
     // Редактирование группы
+    @DisplayName("Редактирование группы")
     @Test
     public void t6EditGroups(){
         Ish ish = new Ish();
         ish.logIn(login, password);
         ish.groups().open();
+        ish.groups().search(editGroup);
         ish.groups().edit();
         ish.groups().clearFieldGroup();
         ish.groups().fillNewCode(newCode);
         ish.groups().save();
-        ish.groups().checkGroupWasChanged(newCode);
-        ish.groups().cancel();
+        ish.groups().clearSerchLine();
+        ish.groups().search(newCode);
         ish.logOut();
     }
 
@@ -113,8 +125,11 @@ public class Groups extends Base {
         Ish ish = new Ish();
         ish.logIn(login, password);
         ish.groups().open();
-        ish.groups().delete();
+        ish.groups().search(deleteGroup);
+        ish.groups().delete(deleteGroup);
+        ish.groups().checkGroupNotExist(deleteGroup);
         ish.logOut();
+
     }
 
     // Проверка невозможности удаления группы, которая используется во взаимодействии
@@ -124,20 +139,22 @@ public class Groups extends Base {
         Ish ish = new Ish();
         ish.logIn(login, password);
         ish.groups().open();
-        ish.groups().searchExistResult(intcode);
-        ish.groups().deleteInteger(intcode);
+        ish.groups().search(intcode);
+        ish.groups().delete(intcode);
+        ish.groups().checkThatGroupInInteger(intcode);
         ish.logOut();
     }
 
     //Поиск группы
+    @DisplayName("Поиск группы по коду")
     @Test
     public void t9SearchGroup(){
         Ish ish = new Ish();
         ish.logIn(login, password);
         ish.groups().open();
-        ish.groups().searchWrongResult();
+        ish.groups().search(existcode);
         ish.groups().clearSearchField();
-        ish.groups().searchExistResult(intcode);
+        ish.groups().checkGroupNotExist("wrongckjk;dfd;sfkkkds;");
         ish.logOut();
 
     }
--- src/test/java/sapasoft/ish/pages/Groups.java
+++ src/test/java/sapasoft/ish/pages/Groups.java
@@ -2,6 +2,7 @@ package sapasoft.ish.pages;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
@@ -12,96 +13,94 @@ import static com.codeborne.selenide.Selenide.*;
 public class Groups {
 
    //Действия
+    @Step("Открыть раздел группы")
     public void open(){
         $(By.xpath("//div/ul/li[3]")).click();
     }
 
+    @Step("Добавить новую группу")
     public void add() {
         $(By.xpath("//button[@title=\"Добавить\"]")).click();
     }
 
+    @Step("Редактировать группу")
     public void edit() {
         $(By.xpath("//tbody/tr[1]/td[3]/div/button[1] ")).click();
     }
 
-    public void delete() {
-        $(By.xpath("//tbody/tr[1]/td[3]/div/button[2]")).shouldBe(exist);
-        String t = $(By.xpath("//tbody/tr[1]/td[1]")).getText();
+    @Step("Удалить группу")
+    public void delete(String group) {
+        //$(By.xpath("//tbody/tr[1]/td[1]")).shouldHave(value(group));
         $(By.xpath("//tbody/tr[1]/td[3]/div/button[2]")).click();
         confirm();
-        $$(By.xpath("//tbody")).findBy(text(t)).shouldNotBe(visible);
-
     }
 
+    @Step("Сохранить изменения")
     public void save() {
         $(By.xpath("//span[text()=\"Сохранить\"]")).click();
     }
 
+    @Step("Отменить изменения")
     public void cancel() {
         $(By.xpath("//span[text()=\"Отмена\"]")).click();
     }
 
 
-    public void deleteInteger(String intcode) {
-
-        $(byText(intcode)).closest("tr/td[3]/div/button[2]").click();
-        confirm();
-        Selenide.switchTo().alert().getText().equals("Удаление группы невозможно. Группа исtttпользуется во взаимодействии");
-        confirm();
-        $(byText(intcode)).shouldBe(visible);
-    }
-
+    @Step("Заполнить поле код группы")
     public void fillCodeOfGroup(String code){
         $(By.xpath("//textarea[@name=\"code\"]")).setValue(code);
     }
 
-    public void fillExistCode() {
-        $(By.xpath("//tbody/tr[2]/td[1]")).shouldBe(exist);
 
-        $(By.xpath("//textarea[@name=\"code\"]")).setValue($(By.xpath("//tbody/tr[2]/td[1]")).getText());
-    }
-
+    @Step ("Ввести новое зачение в поле код")
     public void fillNewCode(String newcode){
         $(By.xpath("//textarea[@name=\"code\"]")).setValue(newcode);
     }
 
+    @Step ("Очистить поле код")
     public void clearFieldGroup(){
         $(By.xpath("//textarea[@name=\"code\"]")).sendKeys(Keys.CONTROL + "a");
         $(By.xpath("//textarea[@name=\"code\"]")).sendKeys(Keys.BACK_SPACE);
     }
 
-    public void searchExistResult(String result) {
-        $(By.xpath("//input[@name=\"search\"]")).setValue(result);
-        $(By.xpath("//tbody/tr[1]")).shouldHave(text(result));
-    }
-
-    public void searchWrongResult() {
-        String wrong = "wrongcode"+ System.currentTimeMillis();
-        $(By.xpath("//input[@name=\"search\"]")).setValue(wrong);
-        $(By.xpath("//tbody/tr[1]")).shouldHave(text("Нет данных"));
-    }
-
+    @Step("Очистить поле поиска")
     public void clearSearchField() {
         $(By.xpath("//input[@name=\"search\"]")).sendKeys(Keys.CONTROL + "a");
         $(By.xpath("//input[@name=\"search\"]")).sendKeys(Keys.BACK_SPACE);
     }
 
 
-    // ПРОВЕРКИ
-    public void checkGroupWasCreated(String code) {
-        $$(By.xpath("//tbody/tr[1]/td[1]")).shouldHave(CollectionCondition.texts(code));
-    }
 
+    @Step("Проверка, что группа с таким кодом уже сущетсвует в системе")
     public void checkGroupExist() {
         $(By.xpath("//form[@action=\"#\"]//p")).shouldBe(text("Группа с таким кодом уже существует в системе"));
     }
 
+    @Step("Проверка, что нельзя создать группу с пустым полем код")
     public void checkThatFieldIsEmpty(){
         $(By.xpath("//form[@action=\"#\"]//p")).shouldBe(text("Поле обязательно для заполнения"));
     }
 
-    public void checkGroupWasChanged(String code) {
-        $(byText(code)).shouldBe(visible);
+    @Step("Поиск группы по коду")
+    public void search(String code) {
+        $(By.xpath("//input[@name=\"search\"]")).setValue(code);
+        $(By.xpath("//tbody/tr[1]")).shouldHave(text(code));
+    }
+
+
+    @Step("Проверка, что группа не существует")
+    public void checkGroupNotExist(String code) {
+        $(By.xpath("//input[@name=\"search\"]")).setValue(code);
+        $(By.xpath("//tbody/tr[1]")).shouldHave(text("Нет данных"));
+    }
+    @Step("Проверка что группа использется во взаимодействии")
+    public void checkThatGroupInInteger(String intcode) {
+        confirm();
+        $(byText(intcode)).shouldBe(visible);
+    }
+
+    public void clearSerchLine(){
+        $(By.xpath("//div[@class=\"MuiFormControl-root MuiTextField-root\"]/div/div[2]/button")).click();
     }
 
 }
